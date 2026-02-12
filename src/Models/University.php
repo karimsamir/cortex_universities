81af4c1dcad56a249253846385ef78f51c5e0e8e
Karim Samir <karim5977@gmail.com>
2026-02-12 13:27:55 +0200

    remove Macroable

--- src/Models/University.php
+++ src/Models/University.php
@@ -16,7 +16,6 @@ use Rinvex\Support\Traits\ValidatingTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 use Rinvex\Support\Traits\HashidsTrait;
-use Rinvex\Support\Traits\Macroable;
 
 /**
  *
@@ -28,8 +27,6 @@ class University extends Model
     use HashidsTrait;
     use LogsActivity;
     use ValidatingTrait;
-    use Macroable;
-
 
     /**
      * The event map for the model.
